2c7c3be9149b53c9ad5d9f155163a6522b3fa18a
sungjoon <qq159aa@gmail.com>
2021-01-22 07:07:14 +0000

    MPU9250 data 수집 코드

---
+++ examples/thread-sampling/run.py
@@ -0,0 +1,75 @@
+#####################################################################
+# Author: Jeferson Menegazzo                                        #
+# Year: 2020                                                        #
+# License: MIT                                                      #
+#####################################################################
+
+from sensors import Sensors
+
+# Simple comand-line interface
+def __init__():
+
+    sensors = None # type: Sensors
+
+    while True:
+
+        print("""
+        [1] Sensors - Create/Recreate
+        [2] Sensors - Apply Configuration
+        [3] Sensors - Reset Configuration
+        [4] Sensors - Calibrate
+        [5] Sampling - Start
+        [6] Sampling - Stop
+        [7] Sampling - View Current
+        [0] Exit
+        """)
+
+        option = input("Choice: ")
+
+        if option == "0":
+
+            if not(sensors is None) and sensors.running:
+                sensors.stop()
+
+            print("Exiting")
+            break
+
+        elif option == "1":
+            sensors = Sensors()
+            print("Sensors created")
+
+        elif sensors is None:
+            print("Not created sensors")
+
+        elif option == "2":
+            sensors.configure()
+            print("Configuration applied to sensors")
+
+        elif option == "3":
+            sensors.reset()
+            print("Sensor configurations reseted")
+
+        elif option == "4":
+            sensors.calibrate()
+            print("Calibrated sensors")
+
+        elif option == "5":
+            sensors.start()
+            print("Sampling started")
+
+        elif option == "6":
+            sensors.stop()
+            print("Sampling stoped")
+
+        elif option == "7":
+            sensors.showCurrent()
+
+        else:
+            print("Invalid Choice.")
+
+if __name__ == "__main__":
+
+    try:
+        __init__()
+    except KeyboardInterrupt:
+        print("Exit")
---
+++ examples/thread-sampling/sampling.py
@@ -0,0 +1,100 @@
+#####################################################################
+# Author: Jeferson Menegazzo                                        #
+# Year: 2020                                                        #
+# License: MIT                                                      #
+#####################################################################
+
+import csv
+import datetime
+import os
+import sys
+import time
+from threading import Thread
+import sys
+sys.path.append("")
+
+from mpu9250_jmdev.mpu_9250 import MPU9250
+
+class Sampling(Thread):
+
+    mpu = None
+    folder = "../data"
+    file = None
+    running = False
+    sleepStart = 5 # In seconds
+    # sampling_rate = 0.01 # 100 Hz
+
+    def __init__(self, address_ak, address_mpu_master, address_mpu_slave, bus, gfs, afs, mfs, mode):
+        Thread.__init__(self)
+        self.mpu = MPU9250(address_ak, address_mpu_master, address_mpu_slave, bus, gfs, afs, mfs, mode)
+    
+    def configure(self):
+        self.mpu.configure()
+
+    def reset(self):
+        self.mpu.reset()
+
+    def calibrate(self):
+        self.mpu.calibrate()
+
+    def getAllData(self):
+        return self.mpu.getAllData()
+
+    def getAllDataLabels(self):
+        return self.mpu.getAllDataLabels()
+
+    def getAllSettings(self):
+        return self.mpu.getAllSettings()
+
+    def getAllSettingsLabels(self):
+        return self.mpu.getAllSettingsLabels()
+
+    def startSampling(self, timeSync):
+
+        if not os.path.exists(self.folder):
+            os.makedirs(self.folder)
+
+        fileSuffix = str(hex(self.mpu.address_mpu_master)) + " " + datetime.datetime.fromtimestamp(timeSync).strftime('%d-%m-%Y %H-%M-%S') + ".csv"
+        self.file = self.folder + "/data-set-mpu-" + fileSuffix
+        settings = self.folder + "/settings-mpu-" + fileSuffix
+        
+        with open(settings, "w+") as csvfile:
+            spamwriter = csv.writer(csvfile, quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            spamwriter.writerow(self.getAllSettingsLabels())
+            spamwriter.writerow(self.getAllSettings())
+        
+        self.timeSync = timeSync
+        self.running = True
+        self.start()
+
+    def stopSampling(self):
+        self.running = False
+        self.join()
+
+    def run(self):
+
+        with open(self.file, "w+") as csvfile:
+            
+            spamwriter = csv.writer(csvfile, quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+            # Writing Labels
+            row = self.getAllDataLabels()
+            spamwriter.writerow(row)
+
+            # Start threads at same time
+            sleepTime = self.sleepStart + (self.timeSync - int(self.timeSync))
+            time.sleep(sleepTime)
+
+            # lastTime = time.time()
+
+            while self.running:
+
+                row = self.getAllData()
+                spamwriter.writerow(row)
+                
+                # sleepTime = self.sampling_rate - (row[0] - lastTime)
+                
+                # if(sleepTime > 0):
+                #     time.sleep(sleepTime)
+
+                # lastTime = row[0]
